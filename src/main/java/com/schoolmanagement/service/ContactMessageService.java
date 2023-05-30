6d6e2cd09551e09661b495161ebd502071289fc6
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-05-30 23:38:16 +0300

    30th may, 2023

--- src/main/java/com/schoolmanagement/service/ContactMessageService.java
+++ src/main/java/com/schoolmanagement/service/ContactMessageService.java
@@ -1,8 +1,12 @@
 package com.schoolmanagement.service;
 
+import com.schoolmanagement.payload.request.ContactMessageRequest;
+import com.schoolmanagement.payload.response.ContactMessageResponse;
 import com.schoolmanagement.repository.ContactMessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor //final fieldlardan cons olusturuyor
@@ -10,4 +14,11 @@ public class ContactMessageService {
 
     private ContactMessageRepository contactMessageRepository;
 
+    //NOT save() methodu **********************
+    public RespsonseMessage<ContactMessageResponse> save(ContactMessageRequest contactMessageRequest) {
+
+        //ayni kisi ayni gun icinde sadece 1 defa mesaj gonderebilsin
+        boolean isSameMessageWithSameEmailForToday =
+                contactMessageRepository.existsByEmailEqualsAndDateEquals(contactMessageRequest.getEmail(), LocalDate.now());
+    }
 }
