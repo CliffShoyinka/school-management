a196bd568ed199e14e783927eb2b932d03aed5e9
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-06-11 23:43:47 +0300

    june 11th, 2023

--- src/main/java/com/schoolmanagement/repository/TeacherRepository.java
+++ src/main/java/com/schoolmanagement/repository/TeacherRepository.java
@@ -1,7 +1,11 @@
 package com.schoolmanagement.repository;
 
 import com.schoolmanagement.entity.concretes.Teacher;
+import com.schoolmanagement.payload.response.TeacherResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     boolean existsByUsername(String username);
@@ -13,4 +17,8 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     Teacher findByUsernameEquals(String username);
 
     boolean existsByEmail(String email);
+
+    //@Query("select t from Teacher t where t.name like concat('%', ?1, '%')")
+    List<Teacher> getTeacherByNameContaining(String teacherName);
+
 }
